617588f92c6175d0f268864887737412388f5cf9
dain <57316771+DainPark-web@users.noreply.github.com>
2022-04-30 00:16:45 +0200

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,22 @@
-export const home = (req, res) => {
+import fetch from "node-fetch"
+import { createApi } from "unsplash-js"
+
+
+const unsplash = createApi({
+    accessKey: process.env.ACCESS_KEY,
+    fetch: fetch,
+  });
+
+export const home = async (req, res) => {
+    unsplash.photos.get({ photoId: 'foo' }).then(result => {
+        if (result.errors) {
+          // handle error here
+          console.log('error occurred: ', result.errors[0]);
+        } else {
+          // handle success here
+          const photo = result.response;
+          console.log(photo);
+        }
+      });
     return res.send("home")
 }
