3d97c8cf5f403e1d47ff90d56f2411f78f21100f
dain <57316771+DainPark-web@users.noreply.github.com>
2022-04-30 11:29:54 +0200

    update

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import 'dotenv/config'
 import express from "express";
+import morgan from 'morgan';
 import homeRouter from "./routers/homeRouter";
 
 const app = express();
 const PORT = 4000;
 
 //pug
+app.use(morgan("dev"))
 app.set('view engine', 'pug');
 app.set("views", process.cwd() + '/src/views');
 
+//frontend
+app.use("client", express.static(__dirname + "/client"))
 
 app.use("/", homeRouter);
 
