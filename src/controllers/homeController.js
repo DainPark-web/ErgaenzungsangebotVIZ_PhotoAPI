6040baf66372686c6e9371547441b3e20e3342ff
dain <57316771+DainPark-web@users.noreply.github.com>
2022-05-11 18:26:15 +0200

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -36,12 +36,13 @@ export const home = async (req, res) => {
    
 }
 
-export const ex01 = (req, res) => {
+export const ex01 = async (req, res) => {
   const { query: {page, search}} = req; 
-    unsplash.search.getPhotos({query: search ? search : "random",page: page ? page : 1, perPage: 30}).then(result => {
+  let maleList;
+  let femaleList;
+  await unsplash.search.getPhotos({query: "male", perPage: 30}).then(result => {
         if (result.errors) {
-          // handle error
-          // Immer zuerst
+       
           console.log('error occurred: ', result.errors[0]);
 
           // Mach hier redirect => "/"
@@ -49,17 +50,38 @@ export const ex01 = (req, res) => {
         } else {
           const feed = result.response;
       
-          // extract total and results array from response
+  
           const { total, results } = feed;
       
           // handle success here
           console.log(`received ${results.length} photos out of ${total}`);
-          // console.log('first photo: ', results);
-          // console.log(page);
-          //rendering home pug
+       
           console.log(results)
-          return res.render("ex01", {photos: results, curPage: page ? page : 1, search: search ? search: "random"})
+          maleList = results
         }
+
       });
+  await unsplash.search.getPhotos({query: "female", perPage: 30}).then(result => {
+        if (result.errors) {
+       
+          console.log('error occurred: ', result.errors[0]);
+
+          // Mach hier redirect => "/"
+          return res.send("error")
+        } else {
+          const feed = result.response;
+      
+  
+          const { total, results } = feed;
+      
+          // handle success here
+          console.log(`received ${results.length} photos out of ${total}`);
+       
+          console.log(results)
+          femaleList = results
+        }
+
+      });
+    return res.render("ex01", {maleList: maleList, femaleList: femaleList, curPage: page ? page : 1, search: "male"})
       
 }
